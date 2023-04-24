306bab45b927911d004cfd0b1ef1f3478d357726
FELIPE MOREIRA DA SILVA <uniffesilva@feinet.fei.edu.br>
2023-04-24 20:52:22 -0300

    Aula de protocolos http

--- servidor.js
+++ servidor.js
@@ -1,15 +1,29 @@
 const port = 3000;
-const http = require("http");
-const express = require("express");
-
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use(express.static("./public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static('./public'));
 
 const server = http.createServer(app);
 
+app.get(["/", "/login"], (req, res) => {
+  res.render("login")
+})
+
+app.post("/cadastrar", (req, res) => {
+  var login = req.body.login
+  var senha = req.body.senha
+  res.render("resposta", {login, senha})
+})
+
 server.listen(port, () => {
-  console.log(`BACKEND is running on port ${port}.`);
+  console.log(`BACKEND is running on port ${port}.`)
 });
 
 module.exports = server;
